a19c84df0c53fe0a2530c64df7dbdaf898c9da7e
Ooooooo972 <mondalmahadev816@gmail.com>
2025-02-26 00:36:09 +0530

    add new line

--- sd.js
+++ sd.js
@@ -1,15 +1,34 @@
-let input =document.getElementById('inputbox');
-let add = document.getElementById ('button');
 
+let input = document.getElementById('inputbox');
+let buttons = document.querySelectorAll('button'); // Assuming buttons have the class "button"
 let string = "";
+
 let arr = Array.from(buttons);
-arr.forEach(button =>{
-    button.addEventistener('click',(e) =>{
-        if(e.target. innerHTML == "="){
-            string =eval(string);
-            input.value = string
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == "=")  
+            {
+            try {
+                string = eval(string);
+                input.value = string;
+            } catch (error) {
+                input.value = "Error";
+            }
+            string = ""; // Reset string after evaluation
+            }
+         else if (e.target.innerHTML == "C") 
+            {
+            string = " ";
+            input.value = string;
+            }else if (e.target.innerHTML == "AC") 
+                {
+                string = "";
+                input.value = string;
+                }
+         else {
+            string += e.target.innerHTML;
+            input.value = string;
         }
-        string += e.target.innerHTML;
-        input.value = string;
-    })
-})
+    });
+});
+
